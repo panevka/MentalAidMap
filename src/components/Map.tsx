ae2428261e54371ff407f914b644f7949e634592
panevka <97028404+panevka@users.noreply.github.com>
2024-12-20 20:18:49 +0100

    Remove boilerplate code, add basic map draft

---
+++ src/components/Map.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <MapContainer center={[52.3, 19.1]} zoom={7} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>This is a popup</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
