97178ef1f3df6a7c3393b2c963f71dbf954249fd
panevka <97028404+panevka@users.noreply.github.com>
2024-12-25 23:10:56 +0100

    Add TopBar, outline MainPage design by adding draft elements.

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,6 @@
 import { TopBar } from "./TopBar";
-import bgImage from "../assets/mental_health.svg";
+import bgImage from "../assets/bg.svg";
+import bgImage2 from "../assets/this.jpg"
 import { CSSProperties } from "react";
 
 const ImageStyles: CSSProperties = {
@@ -7,24 +8,28 @@ const ImageStyles: CSSProperties = {
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
   backgroundPosition: "center",
+  backgroundAttachment: "fixed"
 };
 
 type Props = {};
 export const MainPage = ({ }: Props) => {
   return (
-    <div className="flex flex-col min-h-svh h-full w-screen">
+    <div className="flex flex-col min-h-svh h-svh w-screen" style={ImageStyles}>
+
       <TopBar />
       <main className="flex flex-col flex-1 items-center justify-center">
-        <article className="rounded-3xl h-3/5 w-4/5 bg-[rgba(255,255,255,.25)] backdrop-blur-sm shadow-lg p-8">
+        <div className="rounded-3xl h-3/4 w-4/5 bg-[rgb(255,255,255,0.70)] backdrop-blur-sm shadow-lg p-8">
+          <article className="opacity-100">
 
-          <h1 className="scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">
-            Do Przodu! - platforma pomocowa dla osób w kryzysie
-          </h1>
+            <h1 className="scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">
+              Przed Siebie! - platforma pomocowa dla osób w kryzysie
+            </h1>
 
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Do Przodu! to platforma zawierająca materiały pomocne dla osób przeżywających kryzys. Możesz tu znaleźć:
-          </p>
-        </article>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Przed Siebie! to platforma zawierająca materiały pomocne dla osób przeżywających kryzys. Możesz tu znaleźć:
+            </p>
+          </article>
+        </div>
       </main>
 
       <footer className="bg-slate-500">
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,18 +1,30 @@
 import logo from "../assets/logo.svg";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
 
 type Props = {};
 export const TopBar = ({ }: Props) => {
   return (
-    <header className="flex h-16 p-1 shadow-lg justify-between">
-      <div className="flex flex-row items-center p-2">
-        <img className="h-full m-3" src={logo} alt="logo" />
-        <h1 className="scroll-m-20 text-xl tracking-tight lg:text-6xl">
-          doprzodu.pl
-        </h1>
-      </div>
-      <nav>
+    <header className="flex bg-[rgb(255,255,255)] h-16 p-1 shadow-black shadow-sm rounded-b-3xl">
+      <div className="flex flex-row justify-around h-full w-4/5">
+        <div className="flex flex-row items-center p-2">
+          <img className="h-full m-3" src={logo} alt="logo" />
+          <h1 className="scroll-m-20 text-xl tracking-tight lg:text-6xl">
+            przedsiebie.pl
+          </h1>
+        </div>
 
-      </nav>
+        <nav className="flex flex-1 justify-center items-center">
+          <ul className="flex flex-row w-full justify-around">
+            <li> Infolinie wsparcia </li>
+            <li> Mapa placówek NFZ</li>
+            <li> Mapa aptek </li>
+          </ul>
+        </nav>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.2} stroke="#000000" className="h-full">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
     </header>
   );
 };
