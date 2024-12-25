5bb90de6e1dcbcff3b415086a34eb463633a9b28
panevka <97028404+panevka@users.noreply.github.com>
2024-12-25 17:56:56 +0100

    Improve styling; adjust tags to semantic conventions

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -12,17 +12,22 @@ const ImageStyles: CSSProperties = {
 type Props = {};
 export const MainPage = ({ }: Props) => {
   return (
-    <div className="flex flex-col min-h-svh w-screen">
+    <div className="flex flex-col min-h-svh h-full w-screen">
       <TopBar />
-      <main className="flex flex-col flex-grow items-center">
-        <div className="rounded-3xl h-80 w-4/6 bg-[rgba(255,255,255,.25)] backdrop-blur-sm shadow-lg p-8 self">
-          <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-            Pomóż sobie już dziś
-          </h2>
-        </div>
+      <main className="flex flex-col flex-1 items-center justify-center">
+        <article className="rounded-3xl h-3/5 w-4/5 bg-[rgba(255,255,255,.25)] backdrop-blur-sm shadow-lg p-8">
+
+          <h1 className="scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">
+            Do Przodu! - platforma pomocowa dla osób w kryzysie
+          </h1>
+
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Do Przodu! to platforma zawierająca materiały pomocne dla osób przeżywających kryzys. Możesz tu znaleźć:
+          </p>
+        </article>
       </main>
 
-      <footer>
+      <footer className="bg-slate-500">
         <h1>halo</h1>
         <h1>halo</h1>
         <h1>halo</h1>
