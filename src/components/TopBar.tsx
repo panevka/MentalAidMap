fdc39b2f6602f784dad1bd4919667593588fb82d
panevka <97028404+panevka@users.noreply.github.com>
2024-12-30 19:00:28 +0100

    Make a complete TopBar redesign for all devices.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -11,27 +11,27 @@ export const TopBar = ({ }: Props) => {
 
 
   return (
-    <header className="flex bg-[#2B3A67] h-16 p-2 shadow-slate-400 shadow-lg rounded-b-lg">
+    <header className="flex bg-[#2B3A67] h-16 md:h-20 p-2 justify-center">
       <div className="flex flex-row justify-between h-full w-full">
-        <div className="flex flex-row items-center p-2">
+        <div className="flex flex-row items-center md:mx-4">
           <h1 className="scroll-m-20 text-[#F16A70] text-2xl font-montserrat tracking-tight lg:text-4xl">
             przedsiebie.pl
           </h1>
-          <img className="h-full" src={logo} alt="logo" />
+          <img className="lg:h-full h-1/2 w-auto" src={logo} alt="logo" />
         </div>
 
-        <Navigation navClassName="hidden lg:flex w-full text-[#FAF9F6]" ulClassName="text-base font-medium uppercase font-inter font-16 flex w-full flex-row justify-around items-center" />
+        <Navigation navClassName="hidden md:flex lg:w-3/5 md:mx-5" ulClassName="text-[#FAF9F6] md:text-sm md:text-center lg:text-base md:font-medium uppercase font-inter flex w-full flex-row justify-around items-center" liClassName="md:mx-4" />
 
         <div className={`h-svh w-screen bg-white inset-0 z-40 p-8 fixed flex duration-700 md:hidden ${isSideBarOpen ? "translate-x-0" : "translate-x-full"}`}>
           <div className="flex flex-col p-8 items-center justify-between h-3/5 w-full">
             <button onClick={() => setIsSiderBarOpen(false)} className={` duration-700 ${isSideBarOpen ? "rotate-0" : "rotate-180"}`}>
               <CrossIcon strokeWidth={2.2} />
             </button>
-            <Navigation ulClassName="flex flex-col w-full h-full justify-around items-center" navClassName="h-full w-full" />
+            <Navigation ulClassName="flex flex-col w-full h-4/6 justify-around items-center" navClassName="h-full w-full" />
           </div>
         </div>
 
-        <button onClick={() => setIsSiderBarOpen(true)} className={`h-full flex items-center justify-center duration-150 lg:hidden`}>
+        <button onClick={() => setIsSiderBarOpen(true)} className={`h-full flex items-center justify-center duration-150 md:hidden`}>
           <HamburgerMenuIcon strokeWidth={2.2} strokeColor="#ffffff" IconClassName="size-14" />
         </button>
 
