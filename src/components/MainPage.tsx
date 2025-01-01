10570bd31f43b0f5415943e04ce7dfe36b5e8b4c
panevka <97028404+panevka@users.noreply.github.com>
2025-01-01 17:46:09 +0100

    Replace draft MainPage article & heading with proper one

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -21,36 +21,35 @@ export const MainPage = ({ }: Props) => {
       <main className="flex flex-col lg:flex-row flex-1 justify-center">
         <div className="flex flex-1 py-10 px-5">
           {/* <div className="rounded-3xl h-3/4 w-4/5 bg-[rgb(255,255,255,0.70)] backdrop-blur-sm shadow-lg p-8"> */}
-            <article>
+          <article>
 
-              <h1 className="scroll-m-20 font-poppins text-4xl text-[#2B3A67] font-black tracking-tight lg:text-3xl">
-                Przed Siebie! - Twój przewodnik do szybszej pomocy
-              </h1>
-              <p className="leading-7 [&:not(:first-child)]:mt-6 text-[#495057] font-normal tracking-wider text-sm">
-                  Problemy ze zdrowiem psychicznym nie powinny czekać!
-                <div>
-                  Nasza strona pomoże Ci szybko znaleźć wsparcie:
-                <ul>
-                  <li className="flex flex-row items-baseline">
-                  <MapPinIcon className="size-4 mx-1" aria-hidden="true"/>
-                  <span>Mapa placówek - wszystkie placówki w Polsce,</span>
+            <h1 className="scroll-m-20 font-poppins text-4xl text-[#2B3A67] my-2 font-black tracking-tight lg:text-3xl">
+              Przed Siebie! - Twój przewodnik do szybszej pomocy
+            </h1>
+            <p className="leading-7 text-[#495057] font-normal tracking-wider text-sm my-5">
+              Problemy ze zdrowiem psychicznym nie powinny czekać!
+              <div>
+                Nasza strona pomoże Ci szybko znaleźć wsparcie:
+                <ul className="flex flex-col list-none space-y-4 mt-4">
+                  <li className="flex flex-row items-center space-x-3 height-auto">
+                    <MapPinIcon className="text-[#2B3A67] h-auto max-h-6" aria-hidden="true" />
+                    <span>Mapa placówek - wszystkie placówki w Polsce,</span>
                   </li>
-                  <li className="flex flex-row">
-                  <ClockIcon className="size-4" aria-hidden="true"/>
-                  <span>Aktualne terminy - dostępne wizyty w Twojej okolicy,</span>
+                  <li className="flex items-center space-x-3">
+                    <ClockIcon className="h-5 w-5 text-[#2B3A67]" aria-hidden="true" />
+                    <span className="text-[#495057]">Aktualne terminy - dostępne wizyty w Twojej okolicy,</span>
                   </li>
-                  <li className="flex flex-row">
-                  <GlobeAsiaAustraliaIcon className="size-4" aria-hidden="true"/>
-                  <span>Baza wsparcia - pełna lista dostępnych opcji.</span>
+                  <li className="flex items-center space-x-3">
+                    <GlobeAsiaAustraliaIcon className="h-5 w-5 text-[#2B3A67]" aria-hidden="true" />
+                    <span className="text-[#495057]">Baza wsparcia - pełna lista dostępnych opcji.</span>
                   </li>
                 </ul>
                 <div>
-
                 </div>
 
-                </div>
-              </p>
-            </article>
+              </div>
+            </p>
+          </article>
           {/* </div> */}
         </div>
         <div className="flex flex-1">
