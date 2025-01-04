5c993894b131d5bc80529883886ce6d20fd57371
panevka <97028404+panevka@users.noreply.github.com>
2025-01-04 18:45:05 +0100

    Modify website heading

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -17,9 +17,7 @@ export const MainPage = ({ }: Props) => {
           <article className="flex flex-col lg:py-5 lg:px-10 lg:justify-around">
 
             <h1 className="scroll-m-20 font-poppins text-4xl text-[#2B3A67] my-2 font-black tracking-tight text-center sm:text-6xl sm:my-10 lg:text-6xl lg:leading-snug xl:text-left xl:text-7xl 2xl:text-8xl 2xl:leading-normal">
-              Przed Siebie!
-              <br />
-              Twój przewodnik do szybszej pomocy
+              Twój przewodnik do szybszej pomocy!
             </h1>
             <span className="leading-7 text-[#495057] font-normal tracking-wider text-xs my-5 sm:text-base sm:leading-10 md:leading-10 md:text-xl xl:text-2xl xl:leading-loose">
               Problemy ze zdrowiem psychicznym nie powinny czekać!
