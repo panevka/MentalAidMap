a448efc32aa0b98c5400c82336008088b630a0d9
panevka <97028404+panevka@users.noreply.github.com>
2025-01-06 17:10:30 +0100

    Add routing to App.tsx for Main & Map pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { MainPage } from "./pages/MainPage.tsx";
+import MapPage from "./pages//MapPage.tsx";
 
 export default function App() {
   return (
     <div className="App w-full h-full">
-      <MainPage />
+      <BrowserRouter basename="/MentalAidMap">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/mapa" element={<MapPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
