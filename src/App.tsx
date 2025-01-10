80b714d7ae6720ef4f103d7143335742590fca81
panevka <97028404+panevka@users.noreply.github.com>
2025-01-10 19:40:54 +0100

    Wrap App with QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { MainPage } from "./pages/MainPage.tsx";
 import MapPage from "./pages//MapPage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="App w-full h-full">
-      <BrowserRouter basename="/MentalAidMap">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/mapa" element={<MapPage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter basename="/MentalAidMap">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/mapa" element={<MapPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
