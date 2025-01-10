b23a51c084c0c6d00e43d6b2bfaa80e4fdc4fc6c
panevka <97028404+panevka@users.noreply.github.com>
2025-01-10 17:35:27 +0100

    Adjust Map component default height & width for flexibility

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,7 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Map() {
   return (
-    <MapContainer center={[52.3, 19.1]} zoom={7} scrollWheelZoom={true}>
+    <MapContainer className="w-full h-full" center={[52.3, 19.1]} zoom={7} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
