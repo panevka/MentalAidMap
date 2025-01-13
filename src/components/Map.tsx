d3e5715b2a6c0013ef043d35478629d1394bb536
panevka <97028404+panevka@users.noreply.github.com>
2025-01-13 21:55:11 +0100

    Add functionality of searching facilities on the map.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,35 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Facility } from "@/models/facility";
 
-export default function Map() {
+interface Props {
+  facilities?: Array<Facility>;
+}
+
+function facilityToMarker(facility: Facility, index: number) {
+  const coords = facility.location.coordinates
+  if (!coords) return null;
+
+  return (
+    <Marker key={`i${index}_f${facility["provider-code"]}`} position={[coords[1], coords[0]]}>
+      <Popup>
+        <p>{facility.city},</p>
+        <p>{facility.street} {facility["house-number"]}</p>
+      </Popup>
+    </Marker>
+  )
+}
+
+export default function Map(props: Props) {
+
   return (
     <MapContainer className="w-full h-full" center={[52.3, 19.1]} zoom={7} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>This is a popup</Popup>
-      </Marker>
+      {
+        props.facilities?.map((facility, index) => facilityToMarker(facility, index))
+      }
     </MapContainer>
   );
 }
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,9 +1,33 @@
-import { TopBar } from "../components/TopBar"
+import Map from "../components/Map"
+import { useSearchFacilities } from "@/hooks/useFacilities"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react"
 
 const MapPage = () => {
+	const [city, setCity] = useState('');
+	const [searchCity, setSearchCity] = useState('');
+
+	const { data: facilities, isLoading, error } = useSearchFacilities({ city: searchCity });
+
+	const handleSearch = () => {
+		setSearchCity(city)
+	};
+
 	return (
-		<div>
-			<TopBar />
+		<div className="w-full h-full flex flex-col">
+
+			<div className="bg-white w-full flex flex-row p-3">
+				<Input type="search"
+					placeholder="Znajdź placówki"
+					onChange={(e) => setCity(e.target.value)}
+				/>
+				<Button type="submit" onClick={handleSearch}>Szukaj</Button>
+			</div>
+
+			<div className="w-full h-full">
+				<Map facilities={facilities} />
+			</div>
 		</div>
 	)
 }
