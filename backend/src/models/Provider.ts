5a37b053eb88933a92e2de46ce802d813606443a
panevka <97028404+panevka@users.noreply.github.com>
2025-01-23 20:20:30 +0100

    Modify Provider mongoose schema

--- backend/src/models/Provider.ts
+++ backend/src/models/Provider.ts
@@ -1,38 +1,24 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IProvider extends Document {
-	"provider-code": { type: String, required: true },
-	branch: { type: String, required: true },
-	city: { type: String, required: true },
-	street: { type: String, required: true },
-	"house-number": { type: String, required: true },
-	commune: { type: String, required: true },
-	"post-code": { type: String, required: true },
-	location: {
-		type: { type: String, required: true, enum: ['Point'] },
-		coordinates: {
-			type: [Number],
-			required: true
-		},
-	},
+	code: { type: string, required: true },
+	nip: { type: string, required: true },
+	regon: { type: string, required: true },
+	registry_number: { type: String, required: true },
+	name: { type: string, required: true },
+	phone: { type: string, required: true },
+	agreements: { type: string[], required: true },
 }
 
+
 const ProviderSchema: Schema<IProvider> = new Schema({
-	"provider-code": { type: String, required: true },
-	branch: { type: String, required: true },
-	city: { type: String, required: true },
-	street: { type: String, required: true },
-	"house-number": { type: String, required: true },
-	commune: { type: String, required: true },
-	"post-code": { type: String, required: true },
-	location: {
-		type: { type: String, required: true, enum: ['Point'] },
-		coordinates: {
-			type: [Number],
-			required: true
-		},
-	},
+	code: { type: String, required: true },
+	nip: { type: String, required: true },
+	regon: { type: String, required: true },
+	registry_number: { type: String, required: true },
+	name: { type: String, required: true },
+	phone: { type: String, required: true, default: null },
+	agreements: { type: [String], required: true, default: [] },
 });
 
-ProviderSchema.index({ location: '2dsphere' });
 export default mongoose.model<IProvider>('Provider', ProviderSchema);
