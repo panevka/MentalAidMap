a0386924696ae5c0ce39cd77c7345973aa3f9a7d
panevka <97028404+panevka@users.noreply.github.com>
2025-01-23 21:36:38 +0100

    Create Agreement schema

--- backend/src/models/Provider.ts
+++ backend/src/models/Provider.ts
@@ -49,3 +49,27 @@ const ProviderAddressSchema: Schema<IProviderAddress> = new Schema({
 	location: Location
 });
 export const ProviderAddress = mongoose.model<IProviderAddress>('ProviderAddress', ProviderAddressSchema);
+
+// Agreements collection entry
+export interface IAgreement extends Document {
+	id: { type: string, required: true },
+	code: { type: string, required: true },
+	origin_code: { type: string, required: true },
+	service_type: { type: string, required: true },
+	service_name: { type: string, required: true },
+	amount: { type: Number, required: true },
+	provider_code: { type: string, required: true },
+	year: { type: Number, required: true },
+}
+
+const AgreementSchema: Schema<IAgreement> = new Schema({
+	id: { type: String, required: true },
+	code: { type: String, required: true },
+	origin_code: { type: String, required: true },
+	service_type: { type: String, required: true },
+	service_name: { type: String, required: true },
+	amount: { type: Number, required: true },
+	provider_code: { type: String, required: true },
+	year: { type: Number, required: true },
+});
+export const Agreement = mongoose.model<IAgreement>('Agreement', AgreementSchema);
