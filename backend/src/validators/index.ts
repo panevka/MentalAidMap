97f1f5069f158ede9f30692d194af1a9f8b89e42
panevka <97028404+panevka@users.noreply.github.com>
2025-01-24 18:54:49 +0100

    Create validation for provider endpoint query params

---
+++ backend/src/validators/index.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+const postCodeREG = /^\d{2}-\d{3}$/
+const radiusError = { message: "Radius must be a number between 5 and 150" }
+
+export const providersQueryValidator = z.object({
+	query: z.object({
+		city: z.string({
+			required_error: "City is required"
+		}),
+		postCode: z.string({
+			required_error: "Post-code is required"
+		}).regex(postCodeREG, { message: "Post-code does not match XX-XXX format" }),
+
+		radius: z.coerce.number({
+			required_error: "Radius is required"
+		}).min(5, radiusError).max(150, radiusError)
+
+	})
+})
