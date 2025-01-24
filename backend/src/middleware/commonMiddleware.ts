16e797debfa15e29bea87e647a3ac9ef65a3b10f
panevka <97028404+panevka@users.noreply.github.com>
2025-01-24 16:58:36 +0100

    Export middleware from server.ts to external file

---
+++ backend/src/middleware/commonMiddleware.ts
@@ -0,0 +1,10 @@
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+export const setupCommonMiddleware = (app: any) => {
+	// CORS Middleware
+	app.use(cors());
+
+	// Body Parser Middleware
+	app.use(bodyParser.json());
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,14 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 import mongoose, { ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv'
+import { setupCommonMiddleware } from './middleware/commonMiddleware';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+setupCommonMiddleware(app)
 
 // Database connection
 mongoose
