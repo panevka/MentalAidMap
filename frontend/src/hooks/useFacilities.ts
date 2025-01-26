f0a4b1b368a3d1356df54477bb6b91bfa2870971
panevka <97028404+panevka@users.noreply.github.com>
2025-01-26 16:26:38 +0100

    Create hook for getting provider by code

--- frontend/src/hooks/useFacilities.ts
+++ frontend/src/hooks/useFacilities.ts
@@ -1,11 +1,19 @@
 import { useQuery } from "@tanstack/react-query"
-import { searchFacilities } from "@/api/facilitiesApi"
-import { FacilityAddress, SearchFacilitiesParams } from "@/models/facility";
+import { getFacilityData, searchFacilities } from "@/api/facilitiesApi"
+import { Facility, FacilityAddress, GetFacilityDataParams, SearchFacilitiesParams } from "@/models/facility";
 
 export const useSearchFacilities = (params: SearchFacilitiesParams) => {
 	return useQuery({
 		queryKey: ['facilities', params.city, params.postCode, params.radius],
 		queryFn: async (): Promise<Array<FacilityAddress>> => searchFacilities(params),
 		enabled: !!params.city && !!params.postCode && !!params.radius
+	})
+};
+
+export const useGetFacility = (params: GetFacilityDataParams) => {
+	return useQuery({
+		queryKey: ['facility', params.providerCode],
+		queryFn: async (): Promise<Facility> => getFacilityData(params),
+		enabled: !!params.providerCode
 	})
 };
