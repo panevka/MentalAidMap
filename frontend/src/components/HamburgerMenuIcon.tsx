3ce5a101bc8e6ea54b346c8ff5e9b7c93a35f778
panevka <97028404+panevka@users.noreply.github.com>
2025-02-05 15:12:20 +0100

    refactor: change variable names for HamburgerMenuIcon; add formatting

--- frontend/src/components/HamburgerMenuIcon.tsx
+++ frontend/src/components/HamburgerMenuIcon.tsx
@@ -1,21 +1,28 @@
-type HamburgerMenuIconProps = {
+type Props = {
+  strokeWidth: number;
+  strokeColor?: string;
+  iconClassName?: string;
+};
 
-  strokeWidth: number,
-  strokeColor?: string,
-  IconClassName?: string
-
-}
-
-export const HamburgerMenuIcon: React.FC<HamburgerMenuIconProps> = ({
+export const HamburgerMenuIcon: React.FC<Props> = ({
   strokeWidth,
   strokeColor = "#000000",
-  IconClassName = 'size-14'
+  iconClassName = "size-14",
 }) => {
-
-
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke={strokeColor} className={IconClassName}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke={strokeColor}
+      className={iconClassName}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
     </svg>
-  )
-}
+  );
+};
--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -5,9 +5,8 @@ import { CrossIcon } from "./CrossIcon";
 import { Logo } from "./Logo";
 
 type Props = {};
-export const TopBar = ({ }: Props) => {
+export const TopBar = ({}: Props) => {
   const [isSideBarOpen, setIsSiderBarOpen] = useState<boolean>(false);
-
 
   return (
     <header className="flex bg-[#2B3A67] h-16 md:h-20 p-2 justify-center md:py-10">
@@ -16,21 +15,44 @@ export const TopBar = ({ }: Props) => {
           <Logo tag="h1" className="lg:text-5xl lg:m-3" />
         </div>
 
-        <Navigation navClassName="hidden md:flex lg:w-3/5 md:mx-5" ulClassName="text-[#FAF9F6] md:text-sm md:text-center lg:text-xl md:font-medium uppercase font-inter flex w-full flex-row justify-around items-center" liClassName="md:mx-4 group transition-all duration-300 ease-in-out" linkClassName="bg-bottom bg-gradient-to-r from-white to-white bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out" />
+        <Navigation
+          navClassName="hidden md:flex lg:w-3/5 md:mx-5"
+          ulClassName="text-[#FAF9F6] md:text-sm md:text-center lg:text-xl md:font-medium uppercase font-inter flex w-full flex-row justify-around items-center"
+          liClassName="md:mx-4 group transition-all duration-300 ease-in-out"
+          linkClassName="bg-bottom bg-gradient-to-r from-white to-white bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out"
+        />
 
-        <div className={`h-svh w-screen bg-white inset-0 z-40 p-8 fixed flex duration-700 md:hidden ${isSideBarOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <div
+          className={`h-svh w-screen bg-white inset-0 z-40 p-8 fixed flex duration-700 md:hidden ${
+            isSideBarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
           <div className="flex flex-col p-8 items-center justify-between h-3/5 w-full">
-            <button onClick={() => setIsSiderBarOpen(false)} className={` duration-700 ${isSideBarOpen ? "rotate-0" : "rotate-180"}`}>
+            <button
+              onClick={() => setIsSiderBarOpen(false)}
+              className={` duration-700 ${
+                isSideBarOpen ? "rotate-0" : "rotate-180"
+              }`}
+            >
               <CrossIcon strokeWidth={2.2} />
             </button>
-            <Navigation ulClassName="flex flex-col w-full h-4/6 justify-around items-center" navClassName="h-full w-full" />
+            <Navigation
+              ulClassName="flex flex-col w-full h-4/6 justify-around items-center"
+              navClassName="h-full w-full"
+            />
           </div>
         </div>
 
-        <button onClick={() => setIsSiderBarOpen(true)} className={`h-full flex items-center justify-center duration-150 md:hidden`}>
-          <HamburgerMenuIcon strokeWidth={2.2} strokeColor="#ffffff" IconClassName="size-14" />
+        <button
+          onClick={() => setIsSiderBarOpen(true)}
+          className={`h-full flex items-center justify-center duration-150 md:hidden`}
+        >
+          <HamburgerMenuIcon
+            strokeWidth={2.2}
+            strokeColor="#ffffff"
+            iconClassName="size-14"
+          />
         </button>
-
       </div>
     </header>
   );
