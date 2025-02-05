327a53f4a0877511524216d2cd3ab3736c02c571
panevka <97028404+panevka@users.noreply.github.com>
2025-02-05 15:08:41 +0100

    refactor: rename variables for Cross Icon component; add better formatting

--- frontend/src/components/CrossIcon.tsx
+++ frontend/src/components/CrossIcon.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 
-type CrossIconProps = {
+type Props = {
   strokeWidth: number;
   strokeColor?: string;
-  CrossIconClassName?: string;
+  className?: string;
 };
 
-export const CrossIcon: React.FC<CrossIconProps> = ({
+export const CrossIcon: React.FC<Props> = ({
   strokeWidth,
   strokeColor = "#000000",
-  CrossIconClassName = "size-14"
+  className = "size-14",
 }) => {
-  return (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke={strokeColor} className={CrossIconClassName} >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-  </svg >
-  )
-}
-
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke={strokeColor}
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
