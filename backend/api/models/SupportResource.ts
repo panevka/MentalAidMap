4eca60d155633248e19471282ed5f3c357fe1b67
panevka <97028404+panevka@users.noreply.github.com>
2025-03-19 14:02:14 +0100

    Add explicit names to weekdays for readability

--- backend/api/models/SupportResource.ts
+++ backend/api/models/SupportResource.ts
@@ -9,7 +9,7 @@ export interface ISupportResource extends Document {
     max: { type: number; required: true }
   }
   tags: { type: string[]; required: true }
-  working_hours: { type: WorkingHour[]; required: true }
+  working_hours: { type: IWorkingWeek; required: true }
   type: { type: Type; required: true }
 }
 
@@ -19,7 +19,7 @@ enum Type {
   webchat = 'webchat'
 }
 
-interface WorkingHour {
+interface IWorkingHour {
   from: {
     hour: { type: number; required: true }
     minute: { type: number; required: true }
@@ -30,7 +30,17 @@ interface WorkingHour {
   }
 }
 
-const WorkingHourSchema: Schema<WorkingHour> = new Schema({
+interface IWorkingWeek {
+  monday: { type: IWorkingHour; required: true }
+  tuesday: { type: IWorkingHour; required: true }
+  wednesday: { type: IWorkingHour; required: true }
+  thursday: { type: IWorkingHour; required: true }
+  friday: { type: IWorkingHour; required: true }
+  saturday: { type: IWorkingHour; required: true }
+  sunday: { type: IWorkingHour; required: true }
+}
+
+const WorkingHourSchema: Schema<IWorkingHour> = new Schema({
   from: {
     hour: { type: Number, required: true },
     minute: { type: Number, required: true }
@@ -39,6 +49,16 @@ const WorkingHourSchema: Schema<WorkingHour> = new Schema({
     hour: { type: Number, required: true },
     minute: { type: Number, required: true }
   }
+})
+
+const WorkingWeekSchema: Schema<IWorkingWeek> = new Schema({
+  monday: { type: WorkingHourSchema, required: true },
+  tuesday: { type: WorkingHourSchema, required: true },
+  wednesday: { type: WorkingHourSchema, required: true },
+  thursday: { type: WorkingHourSchema, required: true },
+  friday: { type: WorkingHourSchema, required: true },
+  saturday: { type: WorkingHourSchema, required: true },
+  sunday: { type: WorkingHourSchema, required: true }
 })
 
 const SupportResourceSchema: Schema<ISupportResource> = new Schema(
@@ -50,12 +70,9 @@ const SupportResourceSchema: Schema<ISupportResource> = new Schema(
       maxExclusive: { type: Number, required: true, min: 0 }
     },
     tags: { type: [String], required: true },
-    working_hours: { type: [WorkingHourSchema], required: true, default: [] },
+    working_hours: { type: [WorkingWeekSchema], required: true, default: [] },
     type: { type: Type, required: true }
   },
   { collection: 'SupportResources' }
 )
-export const SupportResource = mongoose.model<ISupportResource>(
-  'SupportResource',
-  SupportResourceSchema
-)
+export const SupportResource = mongoose.model<ISupportResource>('SupportResource', SupportResourceSchema)
