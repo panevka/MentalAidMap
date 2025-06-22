9a8745753c76cb84471bdbd8ed346d76e04ff015
panevka <97028404+panevka@users.noreply.github.com>
2025-06-22 12:17:34 +0200

    chore: integrate eslint with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,75 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    settings: {
+      react: {
+        createClass: "createReactClass", // Regex for Component Factory to use,
+        // default to "createReactClass"
+        pragma: "React", // Pragma to use, default to "React"
+        fragment: "Fragment", // Fragment to use (may be a property of <pragma>), default to "Fragment"
+        version: "detect", // React version. "detect" automatically picks the version you have installed.
+        // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+        // Defaults to the "defaultVersion" setting and warns if missing, and to "detect" in the future
+        defaultVersion: "", // Default React version to use when the version you have installed cannot be detected.
+        // If not provided, defaults to the latest React version.
+        flowVersion: "0.53", // Flow version
+      },
+      propWrapperFunctions: [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        "forbidExtraProps",
+        { property: "freeze", object: "Object" },
+        { property: "myFavoriteWrapper" },
+        // for rules that check exact prop wrappers
+        { property: "forbidExtraProps", exact: true },
+      ],
+      componentWrapperFunctions: [
+        // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
+        "observer", // `property`
+        { property: "styled" }, // `object` is optional
+        { property: "observer", object: "Mobx" },
+        { property: "observer", object: "<pragma>" }, // sets `object` to whatever value `settings.react.pragma` is set to
+      ],
+      formComponents: [
+        // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
+        "CustomForm",
+        { name: "SimpleForm", formAttribute: "endpoint" },
+        { name: "Form", formAttribute: ["registerEndpoint", "loginEndpoint"] }, // allows specifying multiple properties if necessary
+      ],
+      linkComponents: [
+        // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+        "Hyperlink",
+        { name: "MyLink", linkAttribute: "to" },
+        { name: "Link", linkAttribute: ["to", "href"] }, // allows specifying multiple properties if necessary
+      ],
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  eslintConfigPrettier,
 ]);
