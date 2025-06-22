7244d92f07667b2ae31f3fad554850c140b0bc55
panevka <97028404+panevka@users.noreply.github.com>
2025-06-22 22:23:07 +0200

    fix: netlify has build issues

--- apps/frontend/src/components/SvgMap/SvgMap.tsx
+++
@@ -1,7 +0,0 @@
-type SvgMapProps = {
-  className?: string;
-};
-
-export default function SvgMap({ className = "h-full w-full" }: SvgMapProps) {
-  return <div id="map" className={className}></div>;
-}
