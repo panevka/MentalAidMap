d77cb32226a711cbb9b6cc04e8e4ac473817b584
panevka <97028404+panevka@users.noreply.github.com>
2025-07-01 13:23:36 +0200

    feat: add rrule frequency & by day types

---
+++ apps/backend/api/types/RRule.types.ts
@@ -0,0 +1,10 @@
+type RRuleFrequency =
+  | "yearly"
+  | "weekly"
+  | "monthly"
+  | "daily"
+  | "hourly"
+  | "minutely"
+  | "secondly";
+
+type RRuleByDay = "mo" | "tu" | "we" | "th" | "fr" | "sa" | "su";
