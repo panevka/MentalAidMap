6d1dd9e0f739560b2d2d081945897b0d00d0e633
panevka <97028404+panevka@users.noreply.github.com>
2025-07-06 23:04:15 +0200

    fix: replace year & amount type Number with number

--- apps/backend/api/models/Agreement/index.ts
+++ apps/backend/api/models/Agreement/index.ts
@@ -7,9 +7,9 @@ export interface IAgreement extends Document {
   origin_code: { type: string; required: true };
   service_type: { type: string; required: true };
   service_name: { type: string; required: true };
-  amount: { type: Number; required: true };
+  amount: { type: number; required: true };
   provider_code: { type: string; required: true };
-  year: { type: Number; required: true };
+  year: { type: number; required: true };
 }
 
 const AgreementSchema: Schema<IAgreement> = new Schema(
