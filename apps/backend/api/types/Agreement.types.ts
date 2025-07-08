ac70d755bbcecdc1eb71eec78862de9c7895bc9d
panevka <97028404+panevka@users.noreply.github.com>
2025-07-08 18:46:49 +0200

    refactor: decouple IAgreement from Mongoose for flexibility

--- apps/backend/api/types/Agreement.types.ts
+++ apps/backend/api/types/Agreement.types.ts
@@ -1,10 +1,10 @@
-export interface IAgreement extends Document {
-  id: { type: string; required: true };
-  code: { type: string; required: true };
-  origin_code: { type: string; required: true };
-  service_type: { type: string; required: true };
-  service_name: { type: string; required: true };
-  amount: { type: number; required: true };
-  provider_code: { type: string; required: true };
-  year: { type: number; required: true };
+export interface IAgreement {
+  id: string;
+  code: string;
+  origin_code: string;
+  service_type: string;
+  service_name: string;
+  amount: number;
+  provider_code: string;
+  year: number;
 }
