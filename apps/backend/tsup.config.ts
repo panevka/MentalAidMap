b101c29a14b6d1d6679bb20ad773356f9c1edbfd
panevka <97028404+panevka@users.noreply.github.com>
2025-07-13 20:01:00 +0200

    chore: add tsup config

---
+++ apps/backend/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  dts: false,
+  entry: ["api/index.ts"],
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  target: "es2022",
+  minify: false,
+  outDir: "dist",
+  tsconfig: "tsconfig.json",
+});
