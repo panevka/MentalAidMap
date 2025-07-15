597a2ec82b244c5a63775f66fd7e9ad847db3c04
panevka <97028404+panevka@users.noreply.github.com>
2025-07-15 21:24:51 +0200

    style: adjust sizing & colors of search elements for SupportResourcesPage

--- apps/frontend/src/pages/SupportResourcesPage.tsx
+++ apps/frontend/src/pages/SupportResourcesPage.tsx
@@ -7,13 +7,13 @@ const SupportResourcesPage: React.FC = () => {
 
   return (
     <div className="bg-[#F7F4FB]">
-      <div className="w-full flex justify-center p-4 items-center h-16 border-b border-b-slate-400">
+      <div className="w-full flex justify-around py-4 items-center h-16 border-b border-b-slate-400">
         <input
           type="text"
           placeholder="Szukaj"
-          className="h-full w-10/12 rounded-xl border border-solid border-gray-400 p-2"
+          className="h-full w-9/12 rounded-xl border border-solid border-gray-400 p-2"
         />
-        <MagnifyingGlassIcon className="h-full text-slate-500" />
+        <MagnifyingGlassIcon className="h-full text-slate-500 border border-gray-400 bg-white p-1 rounded-full" />
       </div>
       {supportResources?.map((r: ISupportResource) => (
         <>
