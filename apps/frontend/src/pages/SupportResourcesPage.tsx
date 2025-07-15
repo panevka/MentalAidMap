7d54375696036b903852ab71a38a04a21f431023
panevka <97028404+panevka@users.noreply.github.com>
2025-07-15 19:20:45 +0200

    feat: create draft SupportResourcePage

--- apps/frontend/src/pages/SupportResourcesPage.tsx
+++ apps/frontend/src/pages/SupportResourcesPage.tsx
@@ -1,5 +1,51 @@
+import { useGetSupportResources } from "@/hooks/useSupportResources";
+import { ISupportResource } from "@shared/database/SupportResource.types";
+
 const SupportResourcesPage: React.FC = () => {
-  return <div>Test</div>;
+  const { data: supportResources } = useGetSupportResources();
+
+  return (
+    <div>
+      {supportResources?.map((r: ISupportResource) => (
+        <>
+          <p>Provider name: {r.provider_name}</p>
+          <p>Name: {r.name}</p>
+          <p>
+            Age range:{" "}
+            {`${r.age_range.minInclusive} - ${r.age_range.maxExclusive}`}
+          </p>
+          <p>Tags: {r.tags.map((tag) => tag)}</p>
+          <p>
+            Availability patterns:
+            {r.availability.patterns.map((p) => (
+              <>
+                <p>
+                  Rrule: {p.rrule.freq}, {p.rrule.count}, {p.rrule.by_day},{" "}
+                  {p.rrule.interval}
+                </p>
+                <p>
+                  Start time: {p.start_time.hour}:{p.start_time.minute}
+                </p>
+                <p>
+                  End time: {p.end_time.hour}:{p.end_time.minute}
+                </p>
+                <p>
+                  {p.excluded_dates?.map((date) => <p>{date.toString()}</p>)}
+                </p>
+              </>
+            ))}
+          </p>
+          <p>
+            {r.availability?.additional_dates?.map((date) => (
+              <p> {date.toString()}</p>
+            ))}
+          </p>
+          <p>{r.support_type}</p>
+          <hr />
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default SupportResourcesPage;
