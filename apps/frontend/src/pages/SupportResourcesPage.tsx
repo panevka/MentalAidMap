e268946f74fca6ca821d1205233820f346600687
panevka <97028404+panevka@users.noreply.github.com>
2025-07-15 23:23:09 +0200

    feat: remove elements other than name, provider_name, age_range & tags
    Rest of elements will be available in a popup in the future

--- apps/frontend/src/pages/SupportResourcesPage.tsx
+++ apps/frontend/src/pages/SupportResourcesPage.tsx
@@ -24,33 +24,6 @@ const SupportResourcesPage: React.FC = () => {
             {`${r.age_range.minInclusive} - ${r.age_range.maxExclusive}`}
           </p>
           <p>Tags: {r.tags.map((tag) => tag)}</p>
-          <p>
-            Availability patterns:
-            {r.availability.patterns.map((p) => (
-              <>
-                <p>
-                  Rrule: {p.rrule.freq}, {p.rrule.count}, {p.rrule.by_day},{" "}
-                  {p.rrule.interval}
-                </p>
-                <p>
-                  Start time: {p.start_time.hour}:{p.start_time.minute}
-                </p>
-                <p>
-                  End time: {p.end_time.hour}:{p.end_time.minute}
-                </p>
-                <p>
-                  {p.excluded_dates?.map((date) => <p>{date.toString()}</p>)}
-                </p>
-              </>
-            ))}
-          </p>
-          <p>
-            {r.availability?.additional_dates?.map((date) => (
-              <p> {date.toString()}</p>
-            ))}
-          </p>
-          <p>{r.support_type}</p>
-          <hr />
         </div>
       ))}
     </div>
