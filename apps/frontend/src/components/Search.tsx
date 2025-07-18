4421860b1c1953d28655d4d9df5074593cdf0e7b
panevka <97028404+panevka@users.noreply.github.com>
2025-07-18 19:36:17 +0200

    feat: implement filtering of support resources by entered text

--- apps/frontend/src/components/Search.tsx
+++ apps/frontend/src/components/Search.tsx
@@ -1,12 +1,18 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { ChangeEvent } from "react";
 
-const Search = () => {
+interface SearchProps {
+  onSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search = ({ onSearch }: SearchProps) => {
   return (
     <div className="w-full flex justify-around py-4 items-center h-16 border-b border-b-slate-400">
       <input
         type="text"
         placeholder="Szukaj"
         className="h-full w-9/12 rounded-xl border border-solid border-gray-400 p-2"
+        onChange={onSearch}
       />
       <MagnifyingGlassIcon className="h-full text-slate-500 border border-gray-400 bg-white p-1 rounded-full" />
     </div>
--- apps/frontend/src/pages/SupportResourcesPage.tsx
+++ apps/frontend/src/pages/SupportResourcesPage.tsx
@@ -2,15 +2,36 @@ import { useGetSupportResources } from "@/hooks/useSupportResources";
 import { ISupportResource } from "@shared/database/SupportResource.types";
 import SupportResource from "@/components/SupportResource";
 import Search from "@/components/Search";
+import { ChangeEvent, useEffect, useState } from "react";
 
 const SupportResourcesPage: React.FC = () => {
   const { data: supportResources } = useGetSupportResources();
+  const [filteredSupportResources, setFilteredSupportResources] = useState<
+    ISupportResource[]
+  >([]);
+
+  useEffect(() => {
+    setFilteredSupportResources(supportResources || []);
+  }, [supportResources]);
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const searchedTerm = event.target.value;
+    if (!supportResources) return;
+
+    setFilteredSupportResources(
+      supportResources?.filter(
+        (resource) =>
+          resource.provider_name.includes(searchedTerm) ||
+          resource.name.includes(searchedTerm),
+      ),
+    );
+  };
 
   return (
     <>
-      <Search />
+      <Search onSearch={handleSearch} />
       <div className="bg-[#F7F4FB] flex flex-col items-center h-full">
-        {supportResources?.map((r: ISupportResource) => (
+        {filteredSupportResources?.map((r: ISupportResource) => (
           <SupportResource
             name={r.name}
             providerName={r.provider_name}
