76e9f45a15304a272f32981aff2f332e13cba85f
panevka <97028404+panevka@users.noreply.github.com>
2025-07-20 18:40:42 +0200

    [wip] feat: add rrule util for getting first occurence

---
+++ apps/frontend/src/utils/rrule.ts
@@ -0,0 +1,56 @@
+const getFirstRruleOccurence = (dtstart: Date, weekdays: string[]) => {
+  const dtstartDate = new Date(dtstart);
+
+  const dtstartRruleWeekdayName = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+  })
+    .format(dtstartDate)
+    .toLowerCase()
+    .slice(0, 2);
+
+  const obj = new Map([
+    ["mo", 0],
+    ["tu", 1],
+    ["we", 2],
+    ["th", 3],
+    ["fr", 4],
+    ["sa", 5],
+    ["su", 6],
+  ]);
+
+  /**
+   * Sorts an array of weekday names based on their proximity to a reference weekday (`dtstart`).
+   *
+   * Weekdays are ordered from the closest upcoming weekday (without including the same day)
+   * to the furthest, relative to the day of `dtstart`.
+   *
+   * Example:
+   * Given:
+   *   - dtstart occurs on Friday
+   *   - weekdays = ["th", "mo", "fr"]
+   *
+   * the sorted result will be: ["mo", "th", "fr"]
+   * because:
+   *   - "mo" is 3 days after "fr"
+   *   - "th" is 6 days after "fr"
+   *   - "fr" is 0 days after "fr"
+   */
+  const sortedWeekdays = weekdays.sort((a, b) => {
+    const weekdayNum = obj.get(dtstartRruleWeekdayName);
+    const val1 = obj.get(a);
+    const val2 = obj.get(b);
+
+    if (val1 > weekdayNum && val2 > weekdayNum) {
+      if (val1 > val2) return 1;
+      if (val1 < val2) return -1;
+      return 0;
+    }
+
+    if (val1 > weekdayNum) return -1;
+    if (val2 > weekdayNum) return 1;
+
+    if (val1 < val2) return -1;
+    if (val1 > val2) return 1;
+    return 0;
+  });
+};
