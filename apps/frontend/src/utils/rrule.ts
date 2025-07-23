0703ad2e99284d401af4ce66593d7ec073259578
panevka <97028404+panevka@users.noreply.github.com>
2025-07-23 16:24:06 +0200

    refactor: improve overall type safety for getFirstRruleOccurence

--- apps/frontend/src/utils/rrule.ts
+++ apps/frontend/src/utils/rrule.ts
@@ -1,23 +1,23 @@
-const getFirstRruleOccurence = (dtstart: Date, weekdays: string[]) => {
-  const dtstartDate = new Date(dtstart);
+import { RRuleByDay } from "@shared/database/SupportResource.types";
 
-  const dtstartRruleWeekdayName = new Intl.DateTimeFormat("en-US", {
+const getFirstRruleOccurence = (dtstart: Date, weekdays: RRuleByDay[]) => {
+  if (!weekdays || !(weekdays.length >= 0)) throw new Error("Wrong param");
+
+  const dtstartRruleWeekdayName: RRuleByDay = new Intl.DateTimeFormat("en-US", {
     weekday: "long",
   })
-    .format(dtstartDate)
+    .format(dtstart)
     .toLowerCase()
-    .slice(0, 2);
-
-  const obj = new Map([
-    ["mo", 0],
-    ["tu", 1],
-    ["we", 2],
-    ["th", 3],
-    ["fr", 4],
-    ["sa", 5],
-    ["su", 6],
-  ]);
-
+    .slice(0, 2) as RRuleByDay;
+  const obj = {
+    mo: 0,
+    tu: 1,
+    we: 2,
+    th: 3,
+    fr: 4,
+    sa: 5,
+    su: 6,
+  } satisfies Record<RRuleByDay, number>;
   /**
    * Sorts an array of weekday names based on their proximity to a reference weekday (`dtstart`).
    *
@@ -35,10 +35,10 @@ const getFirstRruleOccurence = (dtstart: Date, weekdays: string[]) => {
    *   - "th" is 6 days after "fr"
    *   - "fr" is 0 days after "fr"
    */
-  const sortedWeekdays = weekdays.sort((a, b) => {
-    const weekdayNum = obj.get(dtstartRruleWeekdayName);
-    const val1 = obj.get(a);
-    const val2 = obj.get(b);
+  const sortedWeekdays = weekdays.sort((a: RRuleByDay, b: RRuleByDay) => {
+    const weekdayNum = obj[dtstartRruleWeekdayName];
+    const val1: number = obj[a];
+    const val2: number = obj[b];
 
     if (val1 > weekdayNum && val2 > weekdayNum) {
       if (val1 > val2) return 1;
@@ -56,9 +56,12 @@ const getFirstRruleOccurence = (dtstart: Date, weekdays: string[]) => {
 
   const closestUpcomingWeekday = sortedWeekdays[0];
 
-  const daysToClosestUpcomingWeekday = (dtstartWeekday, upcomingWeekday) => {
-    const dtstartNum = obj.get(dtstartWeekday);
-    const upcomingWeekdayNum = obj.get(upcomingWeekday);
+  const daysToClosestUpcomingWeekday = (
+    dtstartWeekday: RRuleByDay,
+    upcomingWeekday: RRuleByDay,
+  ) => {
+    const dtstartNum = obj[dtstartWeekday];
+    const upcomingWeekdayNum = obj[upcomingWeekday];
 
     if (upcomingWeekdayNum > dtstartNum) {
       return upcomingWeekdayNum - dtstartNum;
@@ -67,4 +70,16 @@ const getFirstRruleOccurence = (dtstart: Date, weekdays: string[]) => {
     }
   };
 
+  function addDaysToDate(currentDate, daysToAdd) {
+    daysToAdd = daysToAdd || 0;
+
+    const futureDate = new Date(currentDate);
+    futureDate.setDate(futureDate.getDate() + daysToAdd);
+    return futureDate;
+  }
+
+  const r = daysToClosestUpcomingWeekday(
+    dtstartRruleWeekdayName,
+    closestUpcomingWeekday,
+  );
 };
