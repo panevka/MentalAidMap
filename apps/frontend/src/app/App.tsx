bbc7a07ad85b0162e71c342569e32bebe1e8b1fa
panevka <97028404+panevka@users.noreply.github.com>
2025-07-23 18:24:19 +0200

    fix: old imports not modified after refactor of directories

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainPage } from "../pages/MainPage.tsx";
-import { MapPage } from "../pages/MapPage.tsx";
+import { MainPage } from "./routes/MainPage.tsx";
+import { MapPage } from "./routes/MapPage.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TopBar } from "../components/TopBar.tsx";
-import SupportResourcesPage from "../pages/SupportResourcesPage.tsx";
+import SupportResourcesPage from "./routes/SupportResourcesPage.tsx";
 import {
   MAIN_PAGE_ROUTE,
   MAP_PAGE_ROUTE,
