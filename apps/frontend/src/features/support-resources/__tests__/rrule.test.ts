e31a1aaa452a8fb52801c53f320fb052c4c92131
panevka <97028404+panevka@users.noreply.github.com>
2025-07-27 19:21:54 +0200

    test: add getFirstRruleOccurence test functionality & description

--- apps/frontend/src/features/support-resources/__tests__/rrule.test.ts
+++ apps/frontend/src/features/support-resources/__tests__/rrule.test.ts
@@ -2,8 +2,11 @@ import { RRuleByDay } from "@shared/database/SupportResource.types";
 import { getFirstRruleOccurence } from "../utils/rrule";
 
 describe("calculates first rrule occurence of a given date", () => {
-  test("", () => {
+  it("should return Sunday as the first occurrence after Wednesday 2025-07-23", () => {
     const weekdays: RRuleByDay[] = ["su", "tu", "mo"];
-    expect(getFirstRruleOccurence(new Date("2025-07-23"), weekdays));
+    const date = new Date("2025-07-23T00:00:00Z"); // wednesday
+    const expected = new Date("2025-07-27T00:00:00Z").getTime(); // sunday
+
+    expect(getFirstRruleOccurence(date, weekdays)).toBe(expected);
   });
 });
