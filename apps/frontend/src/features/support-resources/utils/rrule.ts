fb133e350c6fecf5afb365ba2ff10caf3b7e6cc0
panevka <97028404+panevka@users.noreply.github.com>
2025-08-06 18:46:43 +0200

    feat: create function for finding occurences of week-based rrules

--- apps/frontend/src/features/support-resources/utils/rrule.ts
+++ apps/frontend/src/features/support-resources/utils/rrule.ts
@@ -1,3 +1,4 @@
+import { RRuleByDay } from "@shared/database/SupportResource.types";
 import { DateTime } from "luxon";
 
 export const getDailyOccurences = (
@@ -13,5 +14,60 @@ export const getDailyOccurences = (
     });
 
     return [dtstart, ...getDailyOccurences(nextDate, interval, count - 1)];
+  }
+};
+
+export const getWeeklyOccurences = (
+  dtstart: DateTime,
+  weekdays: RRuleByDay[],
+  interval: number,
+  count: number,
+): DateTime[] => {
+  if (count == 0 || weekdays.length == 0) {
+    return [];
+  } else {
+    const rruleWeekdayMap: Record<RRuleByDay, number> = {
+      mo: 1,
+      tu: 2,
+      we: 3,
+      th: 4,
+      fr: 5,
+      sa: 6,
+      su: 7,
+    };
+    const sortByAscending = (a: DateTime, b: DateTime) =>
+      a < b ? -1 : a > b ? 1 : 0;
+
+    const sameWeekOccurences: DateTime[] = weekdays
+      .map((weekday) => {
+        const weekdayNumber = rruleWeekdayMap[weekday];
+        const dayDiff = weekdayNumber - dtstart.weekday;
+
+        const newOccurence = DateTime.fromMillis(dtstart.toMillis()).plus({
+          days: dayDiff,
+        });
+        return newOccurence;
+      })
+      .filter((date): date is DateTime => date !== null)
+      .sort(sortByAscending);
+
+    const upcomingWeekOccurences = sameWeekOccurences
+      .filter((occurence) => occurence >= dtstart)
+      .slice(0, count);
+
+    const nextReoccurence =
+      upcomingWeekOccurences?.[0]?.plus({
+        weeks: interval,
+      }) || dtstart.plus({ weeks: interval });
+
+    return [
+      ...upcomingWeekOccurences,
+      ...getWeeklyOccurences(
+        nextReoccurence,
+        weekdays,
+        interval,
+        count - upcomingWeekOccurences.length,
+      ),
+    ];
   }
 };
