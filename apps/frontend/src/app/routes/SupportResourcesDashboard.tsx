b69d9f03a2c9da3fab381021233ea939e8865e27
panevka <97028404+panevka@users.noreply.github.com>
2025-08-10 18:07:27 +0200

    feat: add form inputs necessary to match support resource schema

--- apps/frontend/src/app/routes/SupportResourcesDashboard.tsx
+++ apps/frontend/src/app/routes/SupportResourcesDashboard.tsx
@@ -5,9 +5,21 @@ const SupportResourcesDashboard = () => {
         <label htmlFor="name">Nazwa</label>
         <input type="text" name="name" />
 
+        <label htmlFor="description">Opis</label>
+        <input type="text" name="description" />
+
+        <label htmlFor="age-min">Minimalny wiek</label>
+        <input type="number" name="age-min" />
+
+        <label htmlFor="age-max">Maksymalny wiek</label>
+        <input type="number" name="age-max" />
+
         <label htmlFor="provider">Dostawca</label>
         <input type="text" name="provider" />
+      </form>
 
+      {/* Services form */}
+      <form>
         <label htmlFor="type">Typ</label>
         <select name="type">
           <option value="email">E-mail</option>
@@ -15,11 +27,70 @@ const SupportResourcesDashboard = () => {
           <option value="webchat">Czat</option>
         </select>
 
-        <label htmlFor="resource-value">Wartość</label>
-        <input type="text" name="resource-value" />
+        <label htmlFor="contact">Kontakt</label>
+        <input type="text" name="contact" />
 
         <label htmlFor="operator-payment">Opłata operatora</label>
         <input type="checkbox" name="operator-payment" />
+
+        <label htmlFor="responder-profession">Rodzaj pracownika</label>
+        <input type="text" name="responder-profession" />
+
+        <label htmlFor="event">
+          Dodatkowe wydarzenie mające miejsce w trakcie dyżuru
+        </label>
+        <input type="text" name="event" />
+
+        <label htmlFor="timezone">Strefa czasowa</label>
+        <input type="text" name="timezone" />
+
+        <label htmlFor="start-time">Godzina startu</label>
+        <input type="time" name="start-time" />
+
+        <label htmlFor="end-time">Godzina końca</label>
+        <input type="time" name="end-time" />
+
+        <label htmlFor="rrule-frequency">Częstotliwość (format rrule)</label>
+        <input type="text" name="rrule-frequency" />
+
+        <label htmlFor="rrule-count">Liczba wystąpień (format rrule)</label>
+        <input type="number" name="rrule-count" />
+
+        <label htmlFor="rrule-interval">
+          Interwał wystąpień (format rrule)
+        </label>
+        <input type="number" name="rrule-interval" />
+
+        <fieldset>
+          <legend>Według dni (format rrule)</legend>
+
+          <label htmlFor="monday">monday</label>
+          <input type="checkbox" name="monday" value="monday" />
+
+          <label htmlFor="tuesday">tuesday</label>
+          <input type="checkbox" name="tuesday" value="tuesday" />
+
+          <label htmlFor="wednesday">wednesday</label>
+          <input type="checkbox" name="wednesday" value="wednesday" />
+
+          <label htmlFor="thursday">thursday</label>
+          <input type="checkbox" name="thursday" value="thursday" />
+
+          <label htmlFor="friday">friday</label>
+          <input type="checkbox" name="friday" value="friday" />
+
+          <label htmlFor="saturday">saturday</label>
+          <input type="checkbox" name="saturday" value="saturday" />
+
+          <label htmlFor="sunday">sunday</label>
+          <input type="checkbox" name="sunday" value="sunday" />
+        </fieldset>
+
+        <label htmlFor="exluded-dates">Wykluczone daty</label>
+        <input type="date" name="exluded-dates" />
+
+        <label htmlFor="additional-dates">Dodatkowe daty</label>
+        <input type="date" name="additional-dates" />
       </form>
     </div>
   );
