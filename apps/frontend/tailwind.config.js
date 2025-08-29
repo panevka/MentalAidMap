7bd839c75e06607eac168e5a2e6f2fc0bc31b4da
panevka <97028404+panevka@users.noreply.github.com>
2025-08-29 19:22:29 +0200

    fix: use ESM imports instead of require

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -17,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
