30442cd997a99dec56c18b7915236829c4adc96b
panevka <97028404+panevka@users.noreply.github.com>
2025-09-06 12:13:46 +0200

    feat: create draft contact page

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -5,12 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TopBar } from "../components/TopBar.tsx";
 import SupportResourcesPage from "./routes/SupportResourcesPage.tsx";
 import {
+  CONTACT_PAGE_ROUTE,
   MAIN_PAGE_ROUTE,
   MAP_PAGE_ROUTE,
   SUPPORT_RESOURCES_DASHBOARD_PAGE_ROUTE,
   SUPPORT_RESOURCES_PAGE_ROUTE,
 } from "@/config/routeConfig.ts";
 import SupportResourcesDashboard from "./routes/SupportResourcesDashboard.tsx";
+import ContactPage from "./routes/ContactPage.tsx";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -31,6 +33,7 @@ export default function App() {
               path={SUPPORT_RESOURCES_DASHBOARD_PAGE_ROUTE}
               element={<SupportResourcesDashboard />}
             />
+            <Route path={CONTACT_PAGE_ROUTE} element={<ContactPage />} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
