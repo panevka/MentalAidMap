02fc83f9b8bdbc6aaedb8d3631dfb41f5aaebe23
panevka <97028404+panevka@users.noreply.github.com>
2025-09-17 15:14:44 +0200

    feat: create custom class for ContactForm validation

---
+++ apps/email-service/src/main/java/com/mentalaidmap/validator/ValidationException.java
@@ -0,0 +1,7 @@
+package com.mentalaidmap.validator;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
