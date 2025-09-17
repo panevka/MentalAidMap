7b289ff753bcfe608039a5cb55dae574751e2951
panevka <97028404+panevka@users.noreply.github.com>
2025-09-17 14:36:32 +0200

    refactor: change ContactFormDto from class with lombok getter to a record

--- apps/email-service/src/main/java/com/mentalaidmap/dto/ContactFormDTO.java
+++ apps/email-service/src/main/java/com/mentalaidmap/dto/ContactFormDTO.java
@@ -3,25 +3,23 @@ package com.mentalaidmap.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
-@Getter
-public class ContactFormDTO {
+public record ContactFormDTO (
 
 	@NotBlank(message = "Name cannot be blank")
 	@Size(min = 1, max = 80, message = "Full name length should be between 1 and 80 characters")
-	private String fullName;
+	String fullName,
 
 	@NotBlank(message = "Email cannot be blank")
 	@Email(message = "Invalid email format")
 	@Size(min = 6, max = 254, message = "Email address length should be between 6 and 254 characters")
-	private String email;
+	String email,
 
 	@NotBlank(message = "Email subject cannot be blank")
 	@Size(min = 1, max = 80, message = "Subject line should be between 1 and 80 characters")
-	private String subject;
+	String subject,
 
 	@NotBlank(message = "Email body cannot be blank")
 	@Size(min = 1, max = 10240, message = "Email body should be between 1 and 10 240 characters")
-	private String body;
-}
+	String body
+){}
