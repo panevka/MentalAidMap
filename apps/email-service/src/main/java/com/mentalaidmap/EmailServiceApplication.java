8c09e70483bd57be3f7e16d7239475cdf0a6ba73
panevka <97028404+panevka@users.noreply.github.com>
2025-09-20 16:26:47 +0200

    feat: create RateLimitInterceptor & integrate it with the main app

--- apps/email-service/src/main/java/com/mentalaidmap/EmailServiceApplication.java
+++ apps/email-service/src/main/java/com/mentalaidmap/EmailServiceApplication.java
@@ -1,17 +1,26 @@
 package com.mentalaidmap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableCaching
-public class EmailServiceApplication {
+public class EmailServiceApplication implements WebMvcConfigurer {
+
+	@Autowired
+	private RateLimitInterceptor interceptor;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor);
 	}
 
 }
