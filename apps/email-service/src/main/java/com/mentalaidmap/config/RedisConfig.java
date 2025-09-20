0f0b93ed9c29da82b1148de12e77f2e825256c95
panevka <97028404+panevka@users.noreply.github.com>
2025-09-20 16:28:29 +0200

    feat: configure redis template
    List of changes:
    - add explicit typing with Requestor class
    - add Jackson serializer for the value to store class instaces

--- apps/email-service/src/main/java/com/mentalaidmap/config/RedisConfig.java
+++ apps/email-service/src/main/java/com/mentalaidmap/config/RedisConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import com.mentalaidmap.entity.Requestor;
 
 @Configuration
 public class RedisConfig {
@@ -14,10 +20,18 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<?, ?> template = new RedisTemplate<>();
+	public RedisTemplate<String, Requestor> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Requestor> template = new RedisTemplate<String, Requestor>();
 		template.setConnectionFactory(connectionFactory);
+
+		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+		template.setKeySerializer(stringSerializer);
+
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+		template.afterPropertiesSet();
 
 		return template;
 	}
+
 }
