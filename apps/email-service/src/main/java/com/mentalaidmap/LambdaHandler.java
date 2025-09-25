98270ceb8333b438a6a6b334b1bdb21a32ddf358
panevka <97028404+panevka@users.noreply.github.com>
2025-09-25 21:16:31 +0200

    refactor: remove unused import

--- apps/email-service/src/main/java/com/mentalaidmap/LambdaHandler.java
+++ apps/email-service/src/main/java/com/mentalaidmap/LambdaHandler.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
