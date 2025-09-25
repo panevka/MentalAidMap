f1a49bb5d8a8b0f79e90afaab67327edd7fc769c
panevka <97028404+panevka@users.noreply.github.com>
2025-09-25 20:44:12 +0200

    fix: lambda cannot handle incoming requests
    Usage of AwsProxyRequest request type caused issues for AWS Lambda.
    Following error appeared for each request in CloudTrail logs:
    
    ```
    com.amazonaws.serverless.exceptions.InvalidRequestEventException:
        The incoming event is not a valid request from Amazon API Gateway or
    an Application Load Balancer
    ```
    and the request was not handled.
    
    Switching to HttpApiV2ProxyRequest has helped. Solution was found here:
    https://stackoverflow.com/a/78521623.

--- apps/email-service/src/main/java/com/mentalaidmap/LambdaHandler.java
+++ apps/email-service/src/main/java/com/mentalaidmap/LambdaHandler.java
@@ -7,17 +7,18 @@ import java.io.OutputStream;
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 public class LambdaHandler implements RequestStreamHandler {
 
-	private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+	private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
 	static {
 		try {
-			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(EmailServiceApplication.class);
+			handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(EmailServiceApplication.class);
 		} catch (ContainerInitializationException ex) {
 			ex.printStackTrace();
 			throw new RuntimeException("Unable to load spring boot application", ex);
