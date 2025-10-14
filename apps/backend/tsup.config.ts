0938a4458cabd6e77eb59c915d17f3ed63af5bcc
panevka <97028404+panevka@users.noreply.github.com>
2025-10-14 15:15:20 +0200

    fix

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   dts: false,
-  entry: ["src/index.ts"],
+  entry: ["api/index.ts"],
   format: ["cjs"],
   sourcemap: true,
   target: "es2022",
