a0684a5eceef0a50ed01bf493864555f30f3050c
panevka <97028404+panevka@users.noreply.github.com>
2025-10-14 14:28:47 +0200

    build: make tsup do code splitting

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  splitting: true,
   clean: true,
   dts: false,
-  entry: ["api/index.ts"],
+  entry: ["api/**/*.ts"],
   format: ["esm", "cjs"],
   sourcemap: true,
   target: "es2022",
   minify: false,
+  bundle: false,
   outDir: "dist",
   tsconfig: "tsconfig.json",
+  // esbuildOptions(options, context) {
+  //   // the directory structure will be the same as the source
+  //   options.outbase = "./";
+  // },
 });
